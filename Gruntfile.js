b83f77c3cba339694675479796b46d38ed4a239b
Scott Corgan <scottcorgan@gmail.com>
2014-01-02 09:59:50 -0800

    Tests, build, readme set up

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    // browserify: {
+    //   standalone: {
+    //     files: {
+    //       'dist/divshot.js': ['lib/Divshot.js'],
+    //     },
+    //     options: {
+    //       standalone: 'Divshot'
+    //     }
+    //   },
+    //   angular: {
+    //     files: {
+    //       'dist/divshot.angular.js': ['lib/browser/divshot_angular.js'],
+    //     },
+    //     options: {
+    //       ignore: ['promise', 'request']
+    //     }
+    //   }
+    // },
+    
+    // uglify: {
+    //   standalone: {
+    //     src: 'dist/divshot.js',
+    //     dest: 'dist/divshot.min.js'
+    //   },
+    //   angular: {
+    //     src: 'dist/divshot.angular.js',
+    //     dest: 'dist/divshot.angular.min.js'
+    //   }
+    // },
+    
+    // watch: {
+    //   build: {
+    //     files: 'lib/**/*.js',
+    //     tasks: ['build']
+    //   }
+    // }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // grunt.registerTask('build', ['browserify', 'uglify']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-dio-upload
-==========
+# dio-upload
 
 Upload app files to Divshot
+
+## Install
+
+### Server**
+
+```
+npm install dio-upload --save
+```
+
+### Browser
+
+#### Angularjs
+
+Install from the `dist/` directory.
+
+```
+<script src="path/to/dio-upload.angular.js"></script>
+```
+
+Inject module into app
+
+```js
+var app = angular.module('myApp', ['dio-upload']);
+```
+
+#### Standalone
+
+Install from the `dist/` directory.
+
+```
+<script src="path/to/dio-upload.min.js"></script>
+```
+
+## Usage
+
+### Server
+
+```js
+var upload = require('dio-upload');
+
+// Soon
+```
+
+### Browser
+
+#### Angularjs
+
+```js
+// soon
+```
+
+#### Standalone
+
+```js
+// soon
+```
+
+## Run Tests
+
+Runs server side tests and browser tests. Browser tests require that [Phantomjs](http://phantomjs.org) is installed
+
+```
+npm install
+npm test
+```
